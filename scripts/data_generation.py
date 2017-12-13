4d0b1c744c8a4e41773d4a2443f98a0b8230131b
chamauss <charles.maussion@gmail.com>
2017-12-13 15:37:49 +0100

    Images generation

--- scripts/data_generation.py
+++ scripts/data_generation.py
@@ -2,10 +2,27 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-img = Image.open('../data/times/background.png')
+def create_image(text, success):
+    img = Image.open('../data/times/background.png')
 
-draw = ImageDraw.Draw(img)
-font = ImageFont.truetype('./the_brooklyn/The Brooklyn.otf', 16)
-draw.text((0, 0), 'Sample Text', (0, 0, 0), font=font)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype('./Times_New_Roman_Normal.ttf', 80)
+    draw.text((0, 0), text, (0, 0, 0), font=font)
+    string = '../data/times/image' + str(success) + '.png'
+    img.save(string)
 
-img.save('sample-out.jpg')
+success = 0
+
+f = open("text.txt","r")
+for line in f:
+    a = line.split(" ")
+    string = ""
+    while len(string) < 53 and len(a) > 0:
+        string = string + " " + a.pop(0)
+        if len(string) > 49 and len(string) < 53:
+            success += 1
+            create_image(string, success)
+            if success % 100 == 0:
+                print("%i images created"%(success))
+    if success > 1000:
+        break

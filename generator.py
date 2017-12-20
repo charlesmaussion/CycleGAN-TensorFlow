f4424db9d74badec4557028c26766deaf423c4eb
alexisthual <alexisthual@gmail.com>
2017-12-20 09:51:04 +0100

    Comment conv layers dimensions

--- generator.py
+++ generator.py
@@ -22,26 +22,35 @@ class Generator:
         """
         with tf.variable_scope(self.name):
             # conv layers
-            c7s1_32 = ops.c7s1_k(input, self.ngf, is_training=self.is_training, norm=self.norm,
-                                 reuse=self.reuse, name='c7s1_32')                                                      # (?, w, h, 64)
-            d64 = ops.dk(c7s1_32, 2 * self.ngf, is_training=self.is_training, norm=self.norm,
-                         reuse=self.reuse, name='d64')                                                                  # (?, w/2, h/2, 128)
-            d128 = ops.dk(d64, 4 * self.ngf, is_training=self.is_training, norm=self.norm,
-                          reuse=self.reuse, name='d128')                                                                # (?, w/4, h/4, 256)
 
-            res_output = ops.n_res_blocks(d128, reuse=self.reuse, n=9)                                                  # (?, w/4, h/4, 256)
+            # Filters dim: (7, 7, 3); Output dim: (w, h, 64)
+            c7s1_32 = ops.c7s1_k(input, self.ngf, is_training=self.is_training, norm=self.norm,
+                                 reuse=self.reuse, name='c7s1_32')
+            # Filters dim: (3, 3, 64); Output dim: (w/2, h/2, 128)
+            d64 = ops.dk(c7s1_32, 2 * self.ngf, is_training=self.is_training, norm=self.norm,
+                         reuse=self.reuse, name='d64')
+            # Filters dim: (3, 3, 128); Output dim: (w/4, h/4, 256)
+            d128 = ops.dk(d64, 4 * self.ngf, is_training=self.is_training, norm=self.norm,
+                          reuse=self.reuse, name='d128')
+
+            # Filters dim: (3, 3, 256); Output dim: (w/4, h/4, 256)
+            res_output = ops.n_res_blocks(d128, reuse=self.reuse, n=9)
 
             # fractional-strided convolution
+            # Filters dim: (3, 3, 256); Output dim: (w/2, h/2, 128)
             u64 = ops.uk(res_output, 2 * self.ngf, is_training=self.is_training, norm=self.norm,
-                         reuse=self.reuse, name='u64')                                                                  # (?, w/2, h/2, 128)
+                         reuse=self.reuse, name='u64')
+            # Filters dim: (3, 3, 128); Output dim: (w, h, 64)
             u32 = ops.uk(u64, self.ngf, is_training=self.is_training, norm=self.norm,
-                         reuse=self.reuse, name='u32', output_length=self.image_length, output_height=self.image_height)# (?, w, h, 64)
+                         reuse=self.reuse, name='u32', output_length=self.image_length, output_height=self.image_height)
 
             # conv layer
             # Note: the paper said that ReLU and _norm were used
             # but actually tanh was used and no _norm here
+            # Filters dim: (3, 3, 64); Output dim: (w/2, h/2, 3)
             output = ops.c7s1_k(u32, 3, norm=None,
-                    activation='tanh', reuse=self.reuse, name='output')                     # (?, w, h, 3)
+                    activation='tanh', reuse=self.reuse, name='output')
+
         # set reuse=True for next call
         self.reuse = True
         self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=self.name)
